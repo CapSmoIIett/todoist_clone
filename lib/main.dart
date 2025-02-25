41cbfdc4f9b8e214897aef941dbd6c0db98a1eff
CapSmoIIett <vikdrovosek@gmail.com>
2025-02-25 17:02:11 +0300

    change main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
+import 'package:todoist_clone/data/repositories/data_base/db_repositories.dart';
+import 'package:todoist_clone/data/services/task_list/task_list_service.dart';
 import 'package:todoist_clone/ui/home_page/widgets/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (context) => DBProvider),
+        Provider(create: (context) => TaskListService(database: context.read()))
+      ],
+      child: MainApp()
+    )
+    );
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
